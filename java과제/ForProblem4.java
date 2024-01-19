125830e866c7f7be284b8c403160b181bfa9332f
jobumjin <jobumjin5500@gmail.com>
2024-01-19 13:55:40 +0900

    Upgrade code

--- java과제/ForProblem4.java
+++ java과제/ForProblem4.java
@@ -28,5 +28,15 @@ public class ForProblem4 {
 			System.out.println();
 		}
 		
+		System.out.println();
+
+		//다른 방법? if를 빼고?
+		for (int i = 1;i<6; i++) {
+			for (int j = 1;j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
 	}
 }
