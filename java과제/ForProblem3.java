983f685580b43a8e7ef3f9c4c07c73b06da46e50
jobumjin <jobumjin5500@gmail.com>
2024-01-19 14:16:34 +0900

    Update code

--- java과제/ForProblem3.java
+++ java과제/ForProblem3.java
@@ -6,9 +6,13 @@ public class ForProblem3 {
 		 * 1 부터 100중 3, 5, 6의 배수만 출력해보세요.
 		 */
 		for (int i = 1; i <101; i++) {
-			if (i % 3 == 0 && i % 5 == 0 && i % 6 == 0) {
+			//실무에서는 엔터로 띄워 놓기도함
+			if (i % 3 == 0 
+				&& i % 5 == 0
+				&& i % 6 == 0) {
 				System.out.println(i);
 			}
 		}
+		
 	}
 }
