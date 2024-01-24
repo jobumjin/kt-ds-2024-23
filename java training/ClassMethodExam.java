5224b1fbd4ffdab4f338d1fc43b398a98e96f23a
jobumjin <jobumjin5500@gmail.com>
2024-01-24 16:13:16 +0900

    class_method_example

---
+++ java training/ClassMethodExam.java
@@ -0,0 +1,9 @@
+package oop_exam;
+
+public class ClassMethodExam {
+	
+	public static boolean isValidAge(int age, int min, int max) {
+		return age >= min && age <= max;
+	}
+	
+}
---
+++ java training/ClassMethodExamMain.java
@@ -0,0 +1,9 @@
+package oop_exam;
+
+public class ClassMethodExamMain {
+	public static void main(String[] args) {
+		int age = 40;
+		boolean isValidAge = ClassMethodExam.isValidAge(age, 1, 100);
+		System.out.println(isValidAge);
+	}
+}
