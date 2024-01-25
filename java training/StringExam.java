9913ba1380cb0495f3aae1a6ed2962d6aace4e88
jobumjin <jobumjin5500@gmail.com>
2024-01-25 13:29:35 +0900

    String_Method_basic

---
+++ java training/StringExam.java
@@ -0,0 +1,117 @@
+package string_exam;
+
+public class StringExam {
+	
+	public static void main(String[] args) {
+	
+	//contains
+	String address = "서울특별시 서초구 효령로 176";
+	boolean isSeoul = address.contains("서울");
+	System.out.println(isSeoul);
+	
+	//endsWith
+	isSeoul = address.endsWith("176");
+	System.out.println(isSeoul);
+	
+	//equals
+	String name = "ktdsuniversity";
+	boolean isEqual = name.equals("ktdsUniversity");
+	System.out.println(isEqual);
+	
+	//equalsIgnoreCase
+	isEqual = name.equalsIgnoreCase("ktdsUniversity");
+	System.out.println(isEqual);
+	
+	//format
+	String format = "%s에서 교육하는 과정 %s과정";
+	String str = String.format(format, "ktdsuniversity","Java");
+	System.out.println(str);
+	
+	//indexOf
+	String alphabets = "abcdefg";
+	int letterCIndex = alphabets.indexOf("c");
+	System.out.println(letterCIndex);
+	letterCIndex = alphabets.indexOf("C");
+	System.out.println(letterCIndex);
+	letterCIndex = alphabets.indexOf("def");
+	System.out.println(letterCIndex);
+	
+	//isBlank
+	str = "    ";
+	boolean isBlank = str.isBlank();
+	System.out.println(isBlank);
+	
+	//isEmpty
+	str = "    ";
+	boolean isEmpty = str.isEmpty();
+	System.out.println(isEmpty);
+	
+	//join
+	String message = "안녕하세요";
+	name = "홍길동님";
+	String helloMessage = String.join(", ", message, name);
+	System.out.println(helloMessage);
+	
+	//lastIndexOf
+	message = "abcdefgaijkb";
+	int letterALastIndex = message.lastIndexOf("a");
+	System.out.println(letterALastIndex);
+	letterALastIndex = message.lastIndexOf("jj");
+	System.out.println(letterALastIndex);
+	
+	//length
+	message = "abcdefgaijkb";
+	int length = message.length();
+	System.out.println(length);
+	
+	//matches
+	String phone = "01012341234";
+	boolean isNumber = phone.matches("^[0-9]+$");
+	System.out.println(isNumber);
+	
+	//replace
+	message = "안녕하세요, 홍길동님, 안녕히 가세요, 홍길동님.";
+	message = message.replace("홍길동","ktds");
+	System.out.println(message);
+	
+	//replaceAll
+	phone = "010-1234-1234";
+	phone = phone.replaceAll("[^0-9]","");
+	System.out.println(phone);
+	
+	//split
+	phone = "010-1234-1234";
+	String[] phoneArea = phone.split("-");
+	System.out.println(phoneArea[0]);
+	System.out.println(phoneArea[1]);
+	System.out.println(phoneArea[2]);
+	
+	//startsWith
+	phone = "-+82 010-1234-1234";
+	boolean isKoreaNum = phone.startsWith("+82");
+	System.out.println(isKoreaNum);
+	
+	//substring
+	String datetime = "2023-05-02 14:56:13";
+	String year = datetime.substring(0,4);
+	System.out.println(year);
+	String hour = datetime.substring(11,13);
+	System.out.println(hour);
+	String time= datetime.substring(11);
+	System.out.println(time);
+	
+	//trim
+	datetime = "    2023-05-02 14:56:13     ";
+	System.out.println(datetime.length());
+	System.out.println(datetime);
+	datetime = datetime.trim();
+	System.out.println(datetime.length());
+	System.out.println(datetime);
+	
+	String iStr = String.valueOf(1);
+	System.out.println(iStr);
+	
+	}
+	
+	
+}
