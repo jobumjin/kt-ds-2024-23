3321ca2de816ffa804329f7c506a1087ac3eb2f8
jobumjin <jobumjin5500@gmail.com>
2024-01-26 11:30:16 +0900

    CodeUpdate_Array+

--- java training/Customer.java
+++ java training/Customer.java
@@ -10,38 +10,32 @@ public class Customer {
 	/**
 	 * 고객이 가진 상품의 수량
 	 */
-//	int stock;
-	private Product product;
+	private Product[] productArray;
 		
 		
 	public int getWallet() {
 		return this.wallet;
 	}
 
-	public Product getProduct() {
-		return this.product;
+	public Product[] getProductArray() {
+		return this.productArray;
 	}
 
 	/**
 	 * 생성자
 	 */
-//	public Customer(int wallet, int stock) {
 	public Customer(int wallet) {
 		this.wallet = wallet;
-//		this.stock = stock;
-		//프로덕트 생성자 호출
-		this.product = new Product();
+		this.productArray = new Product[3];
 	}
 	
 	/**
 	 * 지출한다.
 	 */
 	public void pay(int price) {
-		//가지고있는 금액빼기 음료가격이 영보다 작으면
 		if (this.wallet - price <= 0) {
 			return; // 메소드 즉시 종료
 		}
-		// Customer이 갖고있던 돈에서 음료값 빼기
 		this.wallet -= price;
 	}
 	
@@ -49,50 +43,66 @@ public class Customer {
 	 * 상품이 증가한다.
 	 */
 	public void addStock(String name, int price) {
-////		this.stock++;
-//		// 고객이 제로콜라를 구매한 적이 있는지? 확인.
-//		// 고객이 제로콜라를 구매한 적이 없다면 
-//		if (this.product.getName() == null) {
-//			// 고객이 가진 상품의 정보를 제로콜라로 채워준다.
-//			this.product.setName(name);
-//			this.product.setPrice(price);
-////			this.product.setQuantity(1);
-//			this.product.setQuantity(VendingMachine.PRODUCT_COUNT);
-//		}
-//		// 고객이 제로콜라를 구매한 적이 있다면.
-//		else {
-//			// 고객이 가진 제로콜라의 수량을 1개 증가 시킨다.
-////			this.product.quantity++;
-//			int quantity = this.product.getQuantity();
-////			quantity++;
-//			quantity += VendingMachine.PRODUCT_COUNT;
-//			this.product.setQuantity(quantity);
-//		}
+
 		this.addStock(name, price, VendingMachine.PRODUCT_COUNT);
 	}
 	
 	// 메소드 오버로딩 
 	public void addStock(String name, int price, int productCount) {
-//		this.stock++;
-		// 고객이 제로콜라를 구매한 적이 있는지? 확인.
-		// 고객이 제로콜라를 구매한 적이 없다면 
-		if (this.product.getName() == null) {
-			// 고객이 가진 상품의 정보를 제로콜라로 채워준다.
-			this.product.setName(name);
-			this.product.setPrice(price);
-//			this.product.setQuantity(1);
-			this.product.setQuantity(productCount);
-		}
-		// 고객이 제로콜라를 구매한 적이 있다면.
-		else {
-			// 고객이 가진 제로콜라의 수량을 1개 증가 시킨다.
-//			this.product.quantity++;
-			int quantity = this.product.getQuantity();
-//			quantity++;
+
+		// 고객이 방금 구매한 제품이 고객의 제품목록(this.productArray)에 있는지 확인한다.
+		// 있다면 productCount만큼 수량만 증가시킨다.
+		// 없다면 비어있는 인덱스를 찾아서 새롭게 할당해 준다. ==> for-each를 사용할 수 없음..
+		Product product = this.getProductByName(name);
+		
+		if(product != null) {
+			int quantity = product.getQuantity();
 			quantity += productCount;
-			this.product.setQuantity(quantity);
+			product.setQuantity(quantity);
+		}
+		else {
+			int nullIndex = this.getNullIndex();
+			if (nullIndex >= 0) {				
+				this.productArray[nullIndex] = new Product();
+				this.productArray[nullIndex].setName(name);
+				this.productArray[nullIndex].setPrice(price);
+				this.productArray[nullIndex].setQuantity(productCount);
+			}
+		}
+		
+	}
+	
+	private Product getProductByName(String name) {
+		
+		for (Product product : this.productArray) {
+			if(product != null && product.getName().equals(name)) {
+				return product;
+			}
+		}
+		
+		return null;
+	}
+	
+	private int getNullIndex() {
+		
+		for (int i = 0; i < this.productArray.length; i++) {
+			if (this.productArray[i] == null) {
+				return i;
+			}
+		}
+		
+		return -1;
+	}
+	
+	public void printProducts() {
+		
+		System.out.println("고객의 잔액: " + this.wallet);
+		for (Product product : this.productArray) {
+			if(product != null) {
+				System.out.println("고객의 상품 수량: " + product.getQuantity());
+				System.out.println("고객의 상품 이름: " + product.getName());
+			}
 		}
 	}
-
 
 }
--- java training/Mart.java
+++ java training/Mart.java
@@ -2,103 +2,33 @@ package vending_machine;
 
 public class Mart {
 	
-	/**
-	 * 함수지향방식으로 개발한 코드
-	 */
-	public static void case1() {
-		
-		// VendingMachine 생성자를 호출해 인스턴스 생성
-		VendingMachine drinkMachine = new VendingMachine();
-
-		// Customer 생성자를 호출해 인스턴스 생성
-		Customer musk = new Customer(0);
-		// mask.wallet에 10000으로 값 재할당
-//		musk.wallet = 10000;
-//		musk.setWallet(10000); setter를 만들면 가능
-//		musk.stock = 0;
-		// 고객이 자판기에 돈을 넣는다.
-		// 고객이 가진 돈을 빼주고
-		musk.pay(1300);
-		
-		// 자판기의 돈을 증가시킨다.
-//		drinkMachine.insertMoney();
-		
-		// 고객이 자판기의 음료 버튼을 누른다.
-		// 자판기의 수량이 하나 줄어들고 
-//		drinkMachine.pressButton();
-		// 고객의 수량이 하나 증가한다.
-		musk.addStock("",0);
-		
-		//결과 출력
-		System.out.println("자판기의 잔액: " + drinkMachine.getMoney());
-		//결과 출력
-		System.out.println("자판기의 상품 수량: " + drinkMachine.getProduct().getQuantity());
-		//결과 출력
-		System.out.println("자판기의 상품 이름: " + drinkMachine.getProduct().getName());
-		//결과 출력
-		System.out.println("고객의 지갑 잔액: " + musk.getWallet());
-		//결과 출력
-		System.out.println("고객의 상품 수량: " + musk.getProduct().getQuantity());
-	
-		
-	}
-	
 	public static void main(String[] args) {
 		
 		// 객체지향방식으로 개발
 		// VendingMachine 생성자를 호출해 인스턴스 생성
 		VendingMachine drinkMachine = new VendingMachine();
-		//아래의 코드는 VendingMachine의 ㅐㅇ성자에서 대체함.
-//		drinkMachine.money = 3000;
-//		drinkMachine.productQuantity = 5;
-		
-		// Customer 생성자를 호출해 인스턴스 생성
 		Customer musk = new Customer(200_000);
-//		아래 코드는 Customer의 생성자에서 대체함
-//		musk.wallet = 10000;
-//		musk.stock = 0;
-		
-		//VendingMachine 생성자를 호출해 생성된 인스턴스 drinkMachine에 insertMoney기능 실행
-		drinkMachine.insertMoney(musk);
-		//VendingMachine 생성자를 호출해 생성된 인스턴스 drinkMachine에 pressButton기능 실행
-		drinkMachine.pressButton(musk);
-		
-		//VendingMachine 생성자를 호출해 생성된 인스턴스 drinkMachine에 insertMoney기능 실행
-		drinkMachine.insertMoney(musk);
-		//VendingMachine 생성자를 호출해 생성된 인스턴스 drinkMachine에 pressButton기능 실행
-		drinkMachine.pressButton(musk);
 
-		//결과출력
-		System.out.println("자판기의 잔액: " + drinkMachine.getMoney());
-		//결과출력
-		System.out.println("자판기의 상품 수량: " + drinkMachine.getProduct().getQuantity());
-		//결과출력
-		System.out.println("자판기의 상품 이름: " + drinkMachine.getProduct().getName());
-		//결과출력
-		System.out.println("고객의 지갑 잔액: " + musk.getWallet());
-		//결과출력
-		System.out.println("고객의 상품 수량: " + musk.getProduct().getQuantity());
+		drinkMachine.insertMoney(musk, "제로펩시");
+		drinkMachine.pressButton(musk, "제로펩시");
+		
+		drinkMachine.insertMoney(musk, "제로콜라");
+		drinkMachine.pressButton(musk, "제로콜라");
+
+		drinkMachine.pringProduct();
+		musk.printProducts();
 		
 		
 		// 메소드 오버로딩 생성자 오버로딩 출력해보기
 		VendingMachine snackMachine = new VendingMachine(400);
 
-		snackMachine.insertMoney(musk);
-		snackMachine.pressButton(musk, 4);
+		snackMachine.insertMoney(musk, "제로스프라이트");
+		snackMachine.pressButton(musk, "제로스프라이트",4);
+		snackMachine.insertMoney(musk, "제로펩시");
+		snackMachine.pressButton(musk, "제로펩시", 2);
 		
-		//결과출력
-		System.out.println("자판기의 잔액: " + snackMachine.getMoney());
-		//결과출력
-		System.out.println("자판기의 상품 수량: " + snackMachine.getProduct().getQuantity());
-		//결과출력
-		System.out.println("자판기의 상품 이름: " + snackMachine.getProduct().getName());
-		//결과출력
-		System.out.println("고객의 지갑 잔액: " + musk.getWallet());
-		//결과출력
-		System.out.println("고객의 상품 수량: " + musk.getProduct().getQuantity());
-				
-		}
+		snackMachine.pringProduct();
+		musk.printProducts();
 	
-
-
+	}
 }
