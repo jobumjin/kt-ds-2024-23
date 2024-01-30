87a8853bb6764a2be73962122e83fc0a797a8dee
BumJin Jo <jobumjin5500@gmail.com>
2024-01-30 16:45:49 +0900

    codeUpdate_논리연산자의 우선순위

--- java training/exception_handling/MemberJoin.java
+++ java training/exception_handling/MemberJoin.java
@@ -16,9 +16,11 @@ public class MemberJoin {
 		System.out.println("ID를 입력하세요.");
 		String id = keyboard.nextLine();
 		
-		if(id != null && id.equals("system") || 
+		// 논리연산자에도 순서가 정해지는데.. 이상해질 수 있음.. 하지만 성공했었고..
+		// 하지만 또.. 이렇게하면 nullPointerror 가 나올 수 있다...
+		if(id != null && (id.equals("system") || 
 						id.equals("root") || 
-						id.equals("admin")) {
+						id.equals("admin"))) {
 			throw new InvalidUserIdException(id + "는 사용할 수 없습니다.");
 		}
 		else if (id != null &&
@@ -30,9 +32,9 @@ public class MemberJoin {
 			throw new UserIdLengthException("ID는 최소 8글자 이상 입력하세요.");
 		}
 //		keyboard.nextLine(); \n\r이 되기에 null이 될 수 없다. 
-//		else if (id == null) {
-//			throw new InvalidUserIdException("ID는 필수 입력 값입니다.");
-//		}
+		else if (id == null) {
+			throw new InvalidUserIdException("ID는 필수 입력 값입니다.");
+		}
 		else {
 			System.out.println(id + "로 가입을 완료했습니다.");
 		}
